beb43b5dc7f53196776ab9b6a33c2becc5980b3c
Luis Zavala <>
2020-04-24 19:08:02 -0400

    ADD test HistoricTable component

--- frontend/src/components/HistoricTable.js
+++ frontend/src/components/HistoricTable.js
@@ -10,16 +10,24 @@ const HistoricTable = ({ historicData }) => {
     return historicData.map((row) => <HistoricTableRow key={row.date} historicRow={row} />);
   };
 
-  return (
-    <table className="table table-striped table-hover table-sm table-dark text-center">
-      <thead>
-        <tr>
-          <th scope="col">Fecha</th>
-          <th scope="col">Valor</th>
-        </tr>
-      </thead>
-      <tbody>{renderRows()}</tbody>
-    </table>
+  const renderTable = () => {
+    return (
+      <table className="table table-striped table-hover table-sm table-dark text-center">
+        <thead>
+          <tr>
+            <th scope="col">Fecha</th>
+            <th scope="col">Valor</th>
+          </tr>
+        </thead>
+        <tbody>{renderRows()}</tbody>
+      </table>
+    );
+  };
+
+  return historicData.length ? (
+    renderTable()
+  ) : (
+    <h2 className="text-center">No se pudieron cargar los datos.</h2>
   );
 };
 
