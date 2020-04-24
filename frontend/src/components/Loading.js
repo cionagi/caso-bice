27e5df5e31972d01ff6ab2d1a79ee1b9eb630a18
Luis Zavala <>
2020-04-23 20:21:33 -0400

    ADD loading component

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <div className="spinner-border" style={{ width: '5rem', height: '5rem' }} role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
