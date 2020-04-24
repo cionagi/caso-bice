fd0e9df9bb035b45470d74d0c343f83343fc8fa1
Luis Zavala <>
2020-04-24 11:56:19 -0400

    UPDATE contianer landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,48 @@
-import React from 'react';
+// Dependency
+import React, { useEffect, useRef, useState } from 'react';
+import moment from 'moment';
+
+// Utils
+import { getValues } from './Utils/actions';
+
+// Components
+import Loading from './components/Loading';
+import RowValue from './components/RowValue';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [dataValue, setDataValue] = useState([]);
+  const ref = useRef(moment().format('DD-MM-YYYY'));
+
+  useEffect(() => {
+    const callGetValues = async () => {
+      const res = await getValues();
+      setLoading(false);
+      setDataValue(res);
+    };
+    callGetValues();
+  }, []);
+
+  const renderValues = () => {
+    return dataValue.map((data) => {
+      return <RowValue valueData={data} key={data.key} />;
+    });
+  };
+
   return (
     <>
-      <nav className="navbar navbar-light bg-light">
+      <nav className="navbar  custom-nav">
         <a className="navbar-brand" href="/">
-          Caso BICE
+          Caso BICE {process.env.BASE_UR}
         </a>
       </nav>
       <div className="container">
-        <h1 className="my-3">Indicadores diarios FECHAAA</h1>
+        <h1 className="my-3">
+          Indicadores diarios
+          <span className="badge badge-custom ml-3">{ref.current}</span>
+        </h1>
+
+        {loading ? <Loading /> : renderValues()}
       </div>
     </>
   );
