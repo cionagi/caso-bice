649316dd1751b0814abdce4c4e5d3ddf9a19638b
Luis Zavala <>
2020-04-24 11:55:12 -0400

    ADD row values components

--- frontend/src/components/RowValue.js
+++ frontend/src/components/RowValue.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import PropTypes from 'prop-types'; // ES6
+// Dependency
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import HistoricValue from './HistoricValue';
+import { getValuesByKey } from '../Utils/actions';
+import LineChartContainer from './LineChartContainer';
 
 const RowValue = ({ valueData }) => {
-  const { key, name, unit, date, value } = valueData;
+  const { key, name, dateParse, valueParse } = valueData;
+  const [openCollapse, setOpenCollapse] = useState(false);
+  const [historicData, setHistoricData] = useState([]);
+
+  const getHistoric = async () => {
+    setOpenCollapse((prev) => !prev);
+    if (!historicData.length) {
+      const res = await getValuesByKey(key);
+      setHistoricData([...res]);
+    }
+  };
+
   return (
     <div className="row">
       <div className="col-12">
-        <div className="border my-2 py-3 px-2 d-flex justify-content-between align-items-baseline bg-light">
-          <div className="text-body">{key}</div>
-          <div className="text-body">{name}</div>
-          <div className="text-body">{unit}</div>
-          <div className="text-body">{date} </div>
-          <div className="text-body">{value} </div>
-          <div>
-            <button type="button" className="btn btn-primary">
-              Ver detalles
-            </button>
+        <div className="border my-2 py-3 px-2 bg-light text-body">
+          <div className="row justify-content-between text-center">
+            <div className="col-12 col-md-3 d-flex align-items-center font-weight-bold">{name}</div>
+            <div className="col-12 col-md-3  d-flex align-items-center">{valueParse}</div>
+            <div className="col-12 col-md-3 col-md-3 d-flex align-items-center">{dateParse}</div>
+            <div className="col-12 col-md-3 d-flex align-items-center">
+              <button className="btn  btn-custom" type="button" onClick={() => getHistoric()}>
+                Historico
+              </button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-12">
+              <HistoricValue historicData={historicData} show={openCollapse} />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12 col-md-12">
+              {!!historicData.length && <LineChartContainer historicData={historicData} />}
+            </div>
           </div>
         </div>
       </div>
