7feac33e7b7effe50815570626e8d673ce8c6d54
Luis Zavala <>
2020-04-26 11:20:42 -0400

    Config deploy

--- frontend/src/__tests__/components/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-
-import App from '../../App';
-
-test('Link renders correctly', () => {
-  const { asFragment } = render(<App />);
-  expect(asFragment()).toMatchSnapshot();
-});
